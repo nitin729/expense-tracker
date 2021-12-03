5b93b29ada0fa9928f5f3cf95974b16e9f7cb622
Rude <rude@pop-os.localdomain>
2021-12-03 15:17:31 +0530

    removed unecessary code

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ date, title, amount }) => {
-  const [newTitle, setTitle] = useState(title);
-  console.log("ExpenseItem evaluated by react");
-  const clickHandler = () => {
-    setTitle("updated");
-    console.log(newTitle);
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{newTitle}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rs.{amount}</div>
       </div>
-      <button onClick={clickHandler}>C M</button>
     </Card>
   );
 };
--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = ({ onAddNewExpense }) => {
